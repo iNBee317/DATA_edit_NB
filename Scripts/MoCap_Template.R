904a418366200ddf7e4c4e1c09dbf4b057077c6c
Nathan <nabaune@gmail.com>
2015-07-30 16:46:00 -0500

    Nathan-End of day
    Need to get notebook and run through a couple of subjects to verify the
    conditions. Once i HAVE THREE SUBJECTS LABELLED BY CONDITION i WILL be
    able to script the analysis. Should be doable for sure.

--- Scripts/MoCap_Template.R
+++ Scripts/MoCap_Template.R
@@ -1,5 +1,5 @@
 #Entry Required: enter the partiicpant number with quotation marks "03_31" or "03_45", etc.
-participant="03_31"
+participant="03_25"
 #----Packages Required (No Entry Required)----
 library(zoo)
 
@@ -20,93 +20,14 @@ optoimport(participant)
  #M = sets the max number of consecutive NA's to interpolate values for
 
 #reference how many trials there are#
-max(as.numeric(keepers$optotrak.pulse.number))
-
-interpolate(N=99,S=1,M=Inf)
 
 
-#create columns for and analyze distance between markers#
-#index and thumb#
+interpolate(N=max(as.numeric(keepers$optotrak.pulse.number)),S=1,M=Inf)
 
-
-
-#creates duplicate x,y,z columns labelled xx,yy,zz an d shifts them up one in order to simplify the calculation for distance
-#create "v" columns for distance measures over time at each marker i.e. velocity#
-v1=c(rep(0,length(inter$x1)));v2=c(rep(0,length(inter$x1)));v3=c(rep(0,length(inter$x1)))
-v4=c(rep(0,length(inter$x1)));v5=c(rep(0,length(inter$x1)));v6=c(rep(0,length(inter$x1)))
-v7=c(rep(0,length(inter$x1)));v8=c(rep(0,length(inter$x1)));v9=c(rep(0,length(inter$x1)))
-v10=c(rep(0,length(inter$x1)));v11=c(rep(0,length(inter$x1)));v12=c(rep(0,length(inter$x1)))
-xx1=inter$x1;yy1=inter$y1;zz1=inter$z1
-xx2=inter$x2;yy2=inter$y2;zz2=inter$z2
-xx3=inter$x3;yy3=inter$y3;zz3=inter$z3
-xx4=inter$x4;yy4=inter$y4;zz4=inter$z4
-xx5=inter$x5;yy5=inter$y5;zz5=inter$z5
-xx6=inter$x6;yy6=inter$y6;zz6=inter$z6
-xx7=inter$x7;yy7=inter$y7;zz7=inter$z7
-xx8=inter$x8;yy8=inter$y8;zz8=inter$z8
-xx9=inter$x9;yy9=inter$y9;zz9=inter$z9
-xx10=inter$x10;yy10=inter$y10;zz10=inter$z10
-xx11=inter$x11;yy11=inter$y11;zz11=inter$z11
-xx12=inter$x12;yy12=inter$y12;zz12=inter$z12
-
-length=length(xx1)
-xx1=xx1[2:length];yy1=yy1[2:length];zz1=zz1[2:length]
-xx2=xx2[2:length];yy2=yy2[2:length];zz2=zz2[2:length]
-xx3=xx3[2:length];yy3=yy3[2:length];zz3=zz3[2:length]
-xx4=xx4[2:length];yy4=yy4[2:length];zz4=zz4[2:length]
-xx5=xx5[2:length];yy5=yy5[2:length];zz5=zz5[2:length]
-xx6=xx6[2:length];yy6=yy6[2:length];zz6=zz6[2:length]
-xx7=xx7[2:length];yy7=yy7[2:length];zz7=zz7[2:length]
-xx8=xx8[2:length];yy8=yy8[2:length];zz8=zz8[2:length]
-xx9=xx9[2:length];yy9=yy9[2:length];zz9=zz9[2:length]
-xx10=xx10[2:length];yy10=yy10[2:length];zz10=zz10[2:length]
-xx11=xx11[2:length];yy11=yy11[2:length];zz11=zz11[2:length]
-xx12=xx12[2:length];yy12=yy12[2:length];zz12=zz12[2:length]
-xx1=c(xx1,0);yy1=c(yy1,0);zz1=c(zz1,0)
-xx2=c(xx2,0);yy2=c(yy2,0);zz2=c(zz2,0)
-xx3=c(xx3,0);yy3=c(yy3,0);zz3=c(zz3,0)
-xx4=c(xx4,0);yy4=c(yy4,0);zz4=c(zz4,0)
-xx5=c(xx5,0);yy5=c(yy5,0);zz5=c(zz5,0)
-xx6=c(xx6,0);yy6=c(yy6,0);zz6=c(zz6,0)
-xx7=c(xx7,0);yy7=c(yy7,0);zz7=c(zz7,0)
-xx8=c(xx8,0);yy8=c(yy8,0);zz8=c(zz8,0)
-xx9=c(xx9,0);yy9=c(yy9,0);zz9=c(zz9,0)
-xx10=c(xx10,0);yy10=c(yy10,0);zz10=c(zz10,0)
-xx11=c(xx11,0);yy11=c(yy11,0);zz11=c(zz11,0)
-xx12=c(xx12,0);yy12=c(yy12,0);zz12=c(zz12,0)
-
-
-analyze=cbind(inter,xx1,yy1,zz1,xx2,xx3,xx4,xx5,xx6,xx7,xx8,xx9,xx10,xx11,xx12,yy1,yy2,yy3,yy4,yy5,yy6,yy7,yy8,yy9,yy10,yy11,yy12,zz1,zz2,zz3,zz4,zz5,zz6,zz7,zz8,zz9,zz10,zz11,zz12,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12)
-rm(xx1,yy1,zz1,xx2,xx3,xx4,xx5,xx6,xx7,xx8,xx9,xx10,xx11,xx12,yy2,yy3,yy4,yy5,yy6,yy7,yy8,yy9,yy10,yy11,yy12,zz2,zz3,zz4,zz5,zz6,zz7,zz8,zz9,zz10,zz11,zz12,v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12)
-
-#calculates distance per marker over time#
-analyze$v1=sqrt((analyze$x1-analyze$xx1)^2+(analyze$y1-analyze$yy1)^2+(analyze$z1-analyze$zz1)^2)/.002
-analyze$v2=sqrt((analyze$x2-analyze$xx2)^2+(analyze$y2-analyze$yy2)^2+(analyze$z2-analyze$zz2)^2)/.002
-analyze$v3=sqrt((analyze$x3-analyze$xx3)^2+(analyze$y3-analyze$yy3)^2+(analyze$z3-analyze$zz3)^2)/.002
-analyze$v4=sqrt((analyze$x4-analyze$xx4)^2+(analyze$y4-analyze$yy4)^2+(analyze$z4-analyze$zz4)^2)/.002
-analyze$v5=sqrt((analyze$x5-analyze$xx5)^2+(analyze$y5-analyze$yy5)^2+(analyze$z5-analyze$zz5)^2)/.002
-analyze$v6=sqrt((analyze$x6-analyze$xx6)^2+(analyze$y6-analyze$yy6)^2+(analyze$z6-analyze$zz6)^2)/.002
-analyze$v7=sqrt((analyze$x7-analyze$xx7)^2+(analyze$y7-analyze$yy7)^2+(analyze$z7-analyze$zz7)^2)/.002
-analyze$v8=sqrt((analyze$x8-analyze$xx8)^2+(analyze$y8-analyze$yy8)^2+(analyze$z8-analyze$zz8)^2)/.002
-analyze$v9=sqrt((analyze$x9-analyze$xx9)^2+(analyze$y9-analyze$yy9)^2+(analyze$z9-analyze$zz9)^2)/.002
-analyze$v10=sqrt((analyze$x10-analyze$xx10)^2+(analyze$y10-analyze$yy10)^2+(analyze$z10-analyze$zz10)^2)/.002
-analyze$v11=sqrt((analyze$x11-analyze$xx11)^2+(analyze$y11-analyze$yy11)^2+(analyze$z11-analyze$zz11)^2)/.002
-analyze$v12=sqrt((analyze$x12-analyze$xx12)^2+(analyze$y12-analyze$yy12)^2+(analyze$z12-analyze$zz12)^2)/.002
-
-d.index.thumb=c(rep(0,length(inter$x1)))
-v.index.thumb=c(rep(0,length(inter$x1)))
-d.index.thumb=sqrt((analyze$x1-analyze$x3)^2+(analyze$y1-analyze$y3)^2+(analyze$z1-analyze$z3)^2)
-analyze=cbind(analyze,d.index.thumb)
-dd.index.thumb=analyze$d.index.thumb
-dd.index.thumb=dd.index.thumb[2:length]
-dd.index.thumb=c(dd.index.thumb,0)
-analyze=cbind(analyze,dd.index.thumb,v.index.thumb)
-analyze$v.index.thumb=sqrt((analyze$d.index.thumb-analyze$dd.index.thumb)^2)/.002
-rm(length)
-
+pythagorean(500)
 
 ##remove bad trials as determined by notes, presentation output, and graph analysis
-bad=c(1:5,63,67:73) #trials to remove
+bad=c(1,27,28,47,52,53,78,84,91,94,99,100,101) #trials to remove
 x=length(bad)
 analyze.remove=analyze
 
@@ -134,15 +55,6 @@ for(i in 1:N){
   abc.v1.low[i]=z[1]
 }
 
-
-cut=analyze.remove[analyze.remove$optotrak.pulse.number==kept.list[1] & analyze.remove$Hz <= abc.v1.low[1],]
-for(i in 2:N){
-x=analyze.remove[analyze.remove$optotrak.pulse.number== kept.list[i] & analyze.remove$Hz <= abc.v1.low[i],]
-cut=rbind(cut,x)
-}
-
-
-
 m.gap=40 # the number of frames after the cutoff at which you avg the distance between the thumb and index to determine cube size
 dis.con=c(rep(0,N)) #placeholder
 for(i in 1:N){
@@ -150,22 +62,11 @@ d=analyze.remove[analyze.remove$optotrak.pulse.number==kept.list[i],]
 d=d$d.index.thumb[d$Hz >= abc.v1.low[i] & d$Hz <= abc.v1.low[i]+m.gap]
 dis.con[i]=mean(d)
 }
-
-for(i in 1:N){
-  graphs.full(num=kept.list[i],x=i)
-}
-
-for(i in kept.list[1]:N){
-  graphs.cut(i)
-}
-
 dis.con.cm=dis.con/10
 
-
-
-
-
-
+for(i in 1:N){
+  graphs.full(num=kept.list[i],x=i,optoLog=F)
+}
 
 
 #import presentation output log to determine good trials
@@ -176,7 +77,7 @@ optoLog$V1=as.character(optoLog$V1)
 optoLog$V1=as.numeric(optoLog$V1)
 
 optoLog.remove=optoLog[optoLog$V1 >= 61 & optoLog$V1 <= 63,]
-a=c(1:99)
+a=c(1:N)
 optoLog.remove=cbind(a,optoLog.remove)
 x=length(bad)
 for (i in 1:x){
@@ -184,6 +85,16 @@ for (i in 1:x){
 }
 
 optoLog.remove=cbind(optoLog.remove,dis.con.cm)
+for(i in 1:N){
+  graphs.full(num=kept.list[i],x=i)
+}
+
+for(i in kept.list[1]:N){
+  graphs.cut(i)
+}
+
+plot(dis.con.cm)
+
 one=optoLog.remove[optoLog.remove$a < 52,]
 two=optoLog.remove[optoLog.remove$a >= 52,]
 
@@ -215,7 +126,11 @@ text( x = 8, y = 1, labels = paste0("Mean 3cm = ",mean(two.3cm$dis.con.cm)), cex
 abline(h=mean(two.3cm$dis.con.cm),col="black")
 
 
-
+cut=analyze.remove[analyze.remove$optotrak.pulse.number==kept.list[1] & analyze.remove$Hz <= abc.v1.low[1],]
+for(i in 2:N){
+  x=analyze.remove[analyze.remove$optotrak.pulse.number== kept.list[i] & analyze.remove$Hz <= abc.v1.low[i],]
+  cut=rbind(cut,x)
+}
 
 
 
